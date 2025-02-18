2c7ba7c89f1eec463e42b2e6803633f3565fa1b5
Josimar Fiuza <e-josimar@weg.net>
2025-02-18 10:10:27 -0300

    criando projeto

---
+++ mercadobitcoin/mercadobitcoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mercadobitcoin
+//
+//  Created by Developer on 18/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mercadobitcoin/mercadobitcoinTests/mercadobitcoinTests.swift
@@ -0,0 +1,17 @@
+//
+//  mercadobitcoinTests.swift
+//  mercadobitcoinTests
+//
+//  Created by Developer on 18/02/25.
+//
+
+import Testing
+@testable import mercadobitcoin
+
+struct mercadobitcoinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
